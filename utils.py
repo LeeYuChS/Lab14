58c83db2ee43a3c4e121fc327f453269f457ae1a
LeeYuChs <lee.alex.tw@gmail.com>
2025-08-23 22:09:02 +0800

    small changed at vit large patch 32

--- utils.py
+++ utils.py
@@ -5,14 +5,14 @@ import shutil
 import json
 import numpy as np
 from matplotlib import pyplot as plt
-from Config import config
+from config import config
 from torch import nn
 from Model import (vit_base_patch16_224_in21k,
                    vit_base_patch32_224_in21k,
                    vit_large_patch16_224_in21k,
                    vit_large_patch32_224_in21k,
                    vit_huge_patch14_224_in21k)
-
+import json
 
 def set_seed(seed):
     """
@@ -33,7 +33,7 @@ def set_seed(seed):
 
 
 
-def create_model(model, num_classes):
+def create_model(model, num_classes, continue_training=None):
     print("using {} model.".format(model))
     if model == "vit_base_patch16_224":
         model = vit_base_patch16_224_in21k(num_classes, has_logits=False)
@@ -42,7 +42,7 @@ def create_model(model, num_classes):
     elif model == "vit_large_patch16_224":
         model = vit_large_patch16_224_in21k(num_classes, has_logits=False)
     elif model == "vit_large_patch32_224":
-        model = vit_large_patch32_224_in21k(num_classes, has_logits=False)
+        model = vit_large_patch32_224_in21k(num_classes, has_logits=False, pretrained=False, continue_weights=continue_training)
     elif model == "vit_huge_patch14_224":
         model = vit_huge_patch14_224_in21k(num_classes, has_logits=False)
     
@@ -69,7 +69,19 @@ def model_parallel(args, model):
     model = nn.DataParallel(model, device_ids=device_ids)
     return model
 
-
+def load_json_data(
+    file_path: str,
+):
+    try:
+        with open(file_path, 'r', encoding='utf-8') as file:
+            data = json.load(file)
+            return data
+    except FileNotFoundError:
+        print(f"Error: Data file not found {file_path}")
+        return []
+    except json.JSONDecodeError as e:
+        print(f"Error: JSON parsing failed - {e}")
+        return []
 
 
 def save_history_json(history, filepath):
@@ -78,6 +90,7 @@ def save_history_json(history, filepath):
 
 
 def plot_history(history, model_name):
+    history = load_json_data(history)
     epochs = range(1, len(history['train_loss']) + 1)
     plt.figure(figsize=(12,5))
 
@@ -101,16 +114,17 @@ def plot_history(history, model_name):
     plt.xlabel('Epoch')
     plt.ylabel('Precision or Recall')
     plt.legend()
-###########################################################################
-    # plt.subplot(1,4,2)
-    # plt.plot(epochs, history['train_acc'], label='Train Acc')
-    # plt.plot(epochs, history['valid_acc'], label='Valid Acc')
-    # plt.xlabel('Epoch')
-    # plt.ylabel('Accuracy')
-    # plt.legend()
-###########################################################################
+# ###########################################################################
+#     # plt.subplot(1,4,2)
+#     # plt.plot(epochs, history['train_acc'], label='Train Acc')
+#     # plt.plot(epochs, history['valid_acc'], label='Valid Acc')
+#     # plt.xlabel('Epoch')
+#     # plt.ylabel('Accuracy')
+#     # plt.legend()
+# ###########################################################################
     plt.tight_layout()
-    plt.savefig(os.path.join(config.save_path, f"{model_name}.jpg"))
+    # plt.savefig(os.path.join(config.save_path, f"{model_name}.jpg"))
+    plt.savefig(os.path.join(r"F:\20250711_backup\Lab_forfun\Lab14-main\checkpoints\2508201040", f"{model_name}.jpg"))
     plt.close()
     # plt.show()
 
